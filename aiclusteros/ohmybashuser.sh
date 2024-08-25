c2bc27f3bfac56f8782fe2aecdec1d8e0876d54a
lunamidori <jaredteam@gmail.com>
2024-08-25 01:11:04 -0700

    oops

--- aiclusteros/ohmybashuser.sh
+++ aiclusteros/ohmybashuser.sh
@@ -5,7 +5,7 @@ case $- in
 esac
 
 # Path to your oh-my-bash installation.
-export OSH='/home/midori-ai/.oh-my-bash/.oh-my-bash'
+export OSH='/home/midori-ai/.oh-my-bash'
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-bash is loaded.
